9f6bf63d10127b55365b9e3eb95028ca258acf22
Bethvour <bethvourc@gmail.com>
2024-05-13 00:42:41 -0500

    about me section

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,16 @@
+export default function AboutMe() {
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--section-img">
+                <img src="" alt="About Me"/>
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <p className="section--title">About</p>
+                    <h1 className="skills-section-heading">About Me</h1>
+                    <p className="hero-section-description">TODO: get description to put here</p>
+                </div>
+            </div>
+        </section>
+    );
+}
